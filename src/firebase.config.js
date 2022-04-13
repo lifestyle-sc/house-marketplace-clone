443bf760de53d217e42a7b73163c344df96c771a
Oluwakayode Momoh <68966387+lifestyle-sc@users.noreply.github.com>
2022-04-13 13:56:25 +0100

    another change

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,18 +1,18 @@
- // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
 
-import {getFirestore} from 'firebase/firestore'
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_GEOCODE_API_KEY,
-  authDomain: "house-marketplace-app-6beb1.firebaseapp.com",
-  projectId: "house-marketplace-app-6beb1",
-  storageBucket: "house-marketplace-app-6beb1.appspot.com",
-  messagingSenderId: "703134756857",
-  appId: "1:703134756857:web:7a4d1dec9462df8d4c1728"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: 'house-marketplace-app-6beb1.firebaseapp.com',
+  projectId: 'house-marketplace-app-6beb1',
+  storageBucket: 'house-marketplace-app-6beb1.appspot.com',
+  messagingSenderId: '703134756857',
+  appId: '1:703134756857:web:7a4d1dec9462df8d4c1728',
 };
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
 
-export const db = getFirestore()
+export const db = getFirestore();
